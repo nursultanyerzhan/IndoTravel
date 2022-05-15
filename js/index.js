060a22f066908564dd5d47b4cf0a33c8600d0132
Nurs <nursultan_erzhan@mail.ru>
2022-05-15 20:24:24 +0600

    tutorial5

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import { timer } from './modules/timer.js';
 import './modules/acc.js';
 import './modules/fly.js';
+import './modules/tourForm.js';
 
 const heroTimer = document.querySelector('.hero__timer');
 const deadline = heroTimer.getAttribute('data-timer-deadline');
